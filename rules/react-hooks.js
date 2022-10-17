34d844d2d282799cb623625c6cb9d5489a4e7bf2
Daniele Debernardi <daniele.debernardi@neolution.ch>
2022-10-17 14:09:00 +0200

    Enable react-hooks rules

---
+++ rules/react-hooks.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "eslint-config-airbnb/hooks",
+  ].map(require.resolve),
+
+  // View link below for react-hooks rules documentation
+  // https://reactjs.org/docs/hooks-rules.html
+  rules: {
+  },
+};
