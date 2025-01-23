3d2448bde643d0ae65dfb3e9a4278fa09993793e
Tobias Merki <103021062+neotob@users.noreply.github.com>
2025-01-22 21:07:44 -1000

    test

--- src/configs/providers/next.mjs
+++ src/configs/providers/next.mjs
@@ -3,7 +3,11 @@ import { FlatCompat } from "@eslint/eslintrc";
 const compat = new FlatCompat();
 const nextRules = [
   ...compat.extends("plugin:@next/next/recommended"),
-  { "@next/next/no-img-element": "off" },
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+    },
+  },
 ];
 
 export default nextRules;
