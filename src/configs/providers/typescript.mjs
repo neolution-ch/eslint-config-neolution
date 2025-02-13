08fab9ffdd6757f49cc44f2c3be40033bd2b83c7
Tobias Merki <103021062+neotob@users.noreply.github.com>
2025-02-12 18:53:26 -1000

    fix

--- src/configs/providers/typescript.mjs
+++ src/configs/providers/typescript.mjs
@@ -3,7 +3,10 @@ import { config, configs } from "typescript-eslint";
 const typescriptRules = config(
   configs.eslintRecommended,
   configs.recommended,
-  configs.recommendedTypeChecked,
+  configs.recommendedTypeChecked.map((rule) => ({
+    ...rule,
+    files: ["**/*.ts", "**/*.tsx", "**/*.mts"], // We use TS config only for TS files
+  })),
   {
     files: ["**/*.js", "**/*.jsx"],
     rules: {
