280032c27a1ecf3976b33f795b2bdc3e7fbbcba6
Tobias Merki <103021062+neotob@users.noreply.github.com>
2025-02-12 19:13:55 -1000

    fix

--- src/configs/providers/typescript.mjs
+++ src/configs/providers/typescript.mjs
@@ -8,6 +8,17 @@ const typescriptRules = config(
     files: ["**/*.ts", "**/*.tsx", "**/*.mts"], // We use TS config only for TS files
   })),
   {
+    files: ["**/*.ts", "**/*.tsx", "**/*.mts"],
+
+    // This is required, see the docs
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  },
+  {
     files: ["**/*.js", "**/*.jsx"],
     rules: {
       "@typescript-eslint/no-require-imports": "off",
