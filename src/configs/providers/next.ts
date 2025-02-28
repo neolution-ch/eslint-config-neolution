d195fd923b331cd0d420702be92221439c0323ba
Tobias Merki <103021062+neotob@users.noreply.github.com>
2025-02-27 22:59:55 -1000

    fix

--- src/configs/providers/next.ts
+++ src/configs/providers/next.ts
@@ -4,11 +4,6 @@ import { InfiniteDepthConfigWithExtends } from "typescript-eslint";
 const compat = new FlatCompat();
 const nextRules = [
   ...compat.extends("plugin:@next/next/recommended"),
-  {
-    rules: {
-      "@next/next/no-img-element": "off",
-    },
-  },
 ] as InfiniteDepthConfigWithExtends;
 
 export default nextRules;
