215093bcb61bfcc6a875002037305ad7af771c80
Tobias Merki <103021062+neotob@users.noreply.github.com>
2025-04-06 19:59:22 -1000

    fix

--- src/configs/providers/react.ts
+++ src/configs/providers/react.ts
@@ -1,5 +1,6 @@
 import { config } from "typescript-eslint";
 import eslintPluginReact from "eslint-plugin-react";
+import globals from "globals";
 
 const reactRulesRecommended = config(
   eslintPluginReact.configs.flat.recommended,
@@ -17,11 +18,26 @@ const reactRulesRecommended = config(
   },
 );
 
-const reactRulesJsx = config(eslintPluginReact.configs.flat["jsx-runtime"], {
-  rules: {
-    "react/jsx-filename-extension": ["error", { extensions: [".tsx", ".jsx"] }],
-    "react/jsx-no-useless-fragment": ["error"],
+const reactRulesJsx = config(
+  eslintPluginReact.configs.flat["jsx-runtime"],
+  {
+    rules: {
+      "react/jsx-filename-extension": [
+        "error",
+        { extensions: [".tsx", ".jsx"] },
+      ],
+      "react/jsx-no-useless-fragment": ["error"],
+    },
   },
-});
+  {
+    languageOptions: {
+      ...eslintPluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
+);
 
 export { reactRulesJsx, reactRulesRecommended };
